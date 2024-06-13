7458214e7ee249e3449c56526d8a703b70f59e93
Febrico Jonata <riko9647@gmail.com>
2024-06-13 23:38:00 +0700

    feat: add auth

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import chatbotRouter from "./chatbot.js";
 import sentimentAnalysisRouter from "./sentiment-analysis.js";
+import usersRouter from "./user.js";
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(cors());
 app.get("/", (req, res) => res.send("Express on Vercel Server"));
 app.use("/api/chat", chatbotRouter);
 app.use("/api/analyze", sentimentAnalysisRouter);
+app.use("/api/users", usersRouter);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
---
+++ api/user.js
@@ -0,0 +1,125 @@
+import express from "express";
+import { config as dotenvConfig } from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+dotenvConfig();
+const usersRouter = express.Router();
+
+const db = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+usersRouter.get("/", async (req, res) => {
+  try {
+    const { email } = req.body;
+    let fetchUsers;
+
+    if (email) {
+      fetchUsers = await db
+        .from("m_users")
+        .select("user_id, name, email")
+        .eq("email", email);
+    } else {
+      fetchUsers = await db.from("m_users").select("user_id, name, email");
+    }
+
+    return res.status(200).json({
+      body: fetchUsers,
+    });
+  } catch (error) {
+    console.error("Error retrieving users:", error.message);
+    return res.status(500).json({
+      status: 500,
+      error: "Internal server error",
+    });
+  }
+});
+
+usersRouter.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    // Check if the user already exists
+    const { data: existingUsers } = await db
+      .from("m_users")
+      .select("*")
+      .eq("email", email);
+
+    if (existingUsers && existingUsers.length > 0) {
+      return res.status(400).json({ error: "User already exists." });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Insert new user into Supabase users table
+    const { data: newUser } = await db
+      .from("m_users")
+      .insert([
+        {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      ])
+      .select("user_id, name, email");
+
+    return res
+      .status(200)
+      .json({ message: "User signed up successfully.", data: newUser });
+  } catch (error) {
+    console.error("Error signing up user:", error.message);
+    return res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+usersRouter.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Fetch user data from Supabase table
+    const { data: users, error } = await db
+      .from("m_users")
+      .select("user_id, name, email")
+      .eq("email", email)
+      .limit(1);
+
+    if (error || !users || users.length === 0) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized, incorrect email or password." });
+    }
+
+    const user = users[0];
+
+    // Compare the provided password with the hashed password stored in the database
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized, incorrect email or password." });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "36h",
+    });
+
+    // Return user data and JWT token
+    return res.status(200).json({
+      message: "User signed in successfully.",
+      user: {
+        user_id: user.user_id,
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    });
+  } catch (error) {
+    console.error("Error signing in user:", error.message);
+    return res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+export default usersRouter;
