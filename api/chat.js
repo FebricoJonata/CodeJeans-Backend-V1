250c523a6d5982f75fd20c2c7083e6fdaba30ef3
FebricoJonata <febrico.jonata@kawanlamaretail.com>
2024-06-15 21:31:15 +0700

    chat room name

--- api/chat.js
+++ api/chat.js
@@ -40,6 +40,31 @@ chatRouter.post("/chat-rooms", verifyToken, async (req, res) => {
   }
 });
 
+chatRouter.post("/chat-room-name", verifyToken, async (req, res) => {
+  try {
+    const { chat_room_id } = req.body;
+    let data;
+
+    if (chat_room_id) {
+      data = await db
+        .from("m_chat_room")
+        .select("*")
+        .eq("chat_room_id", chat_room_id);
+    }
+
+    return res.status(200).json({
+      status: 200,
+      body: data,
+    });
+  } catch (error) {
+    console.error("Error retrieving Chat Room:", error.message);
+    return res.status(500).json({
+      status: 500,
+      error: "Internal server error",
+    });
+  }
+});
+
 chatRouter.post("/all", verifyToken, async (req, res) => {
   try {
     const { chat_room_id } = req.body;
