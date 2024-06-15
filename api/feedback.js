2a36acb62551c147fb09d648f3cf8889873d69e6
FebricoJonata <febrico.jonata@kawanlamaretail.com>
2024-06-15 21:06:36 +0700

    fix: add get feedback all

--- api/feedback.js
+++ api/feedback.js
@@ -31,4 +31,27 @@ feedbackRouter.post("/all", verifyToken, async (req, res) => {
   }
 });
 
+feedbackRouter.get("/all", verifyToken, async (req, res) => {
+  try {
+    const { user_id } = req.body;
+    let data;
+
+    if (user_id) {
+      data = await db.from("t_feedback").select("*").eq("user_id", user_id);
+    } else {
+      data = await db.from("t_feedback").select("*");
+    }
+
+    return res.status(200).json({
+      body: data,
+    });
+  } catch (error) {
+    console.error("Error retrieving feedback:", error.message);
+    return res.status(500).json({
+      status: 500,
+      error: "Internal server error",
+    });
+  }
+});
+
 export default feedbackRouter;
